4e81a2bbd6556932e6d3302f37ce34c8eb4fdc12
sanix sandel <sanicksikani@gmail.com>
2020-09-06 18:31:58 +0300

    Delete Recipe

--- src/main/java/com/sanix/RecipeApp/controllers/RecipeController.java
+++ src/main/java/com/sanix/RecipeApp/controllers/RecipeController.java
@@ -2,10 +2,12 @@ package com.sanix.RecipeApp.controllers;
 
 import com.sanix.RecipeApp.commands.RecipeCommand;
 import com.sanix.RecipeApp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -15,12 +17,14 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
 
+    @GetMapping
     @RequestMapping("recipe/new")
     public String newRecipe(Model model){
         model.addAttribute("recipe", new RecipeCommand());
@@ -28,16 +32,27 @@ public class RecipeController {
         return "recipe/recipeform";
     }
 
+    @GetMapping
     @RequestMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return "recipe/recipeform";
     }
 
-    @PostMapping("recipe")
+    @PostMapping
+    @RequestMapping("recipe")
     public String savedOrUpdate(@ModelAttribute RecipeCommand command){
         RecipeCommand savedCommand=recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/show/"+savedCommand.getId();
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id){
+        log.debug("Deleting id: "+id);
+
+        recipeService.deleteById(Long.valueOf(id));
+        return "redirect:/";
     }
 }
--- src/main/java/com/sanix/RecipeApp/services/RecipeServiceImpl.java
+++ src/main/java/com/sanix/RecipeApp/services/RecipeServiceImpl.java
@@ -63,4 +63,9 @@ public class RecipeServiceImpl implements RecipeService{
         return recipeToRecipeCommand.convert(savedRecipe);
     }
 
+    @Override
+    public void deleteById(Long idToDelete){
+        recipeRepository.deleteById(idToDelete);
+    }
+
 }
